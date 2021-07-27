8194743f1519dcceec429ba8bc70df13d467becf
SeanActon <60246853+SeanActon@users.noreply.github.com>
2021-07-27 11:59:39 -0400

    Add files via upload

---
+++ test_string_tokenizer.py
@@ -0,0 +1,43 @@
+import string_tokenizer
+
+def test_split_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ""
+    #regex = ""
+    expResult = []
+    result = None
+    cut = string_tokenizer.TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit )#, regex)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = string_tokenizer.TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_one_string_result_array_of_one(self):
+    # arrange
+    stringToSplit = "java"
+    regex = ",\\ *"
+    expResult = ["java"]
+    result = None
+    cut = string_tokenizer.TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
